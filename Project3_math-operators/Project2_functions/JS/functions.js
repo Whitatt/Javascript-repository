d38110c22fbc5e3648f9e01c42fe520484ce5019
Whitatt <60752908+Whitatt@users.noreply.github.com>
2021-07-01 20:36:25 -0700

    javascript projects

---
+++ Project3_math-operators/Project2_functions/JS/functions.js
@@ -0,0 +1,5 @@
+function myFunction() {
+    var sentence = "Iam learning";
+    sentence += "a lot from this book!";
+    document.getElementById("Concatenante").innerHTML = sentence;
+}
---
+++ Project3_math-operators/Project2_functions/JS/primary.js
@@ -0,0 +1,5 @@
+function plusequals() {
+    str1 = "this is some";
+    str1 += " text here";
+    document.getElementById("result1").innerHTML = str1;
+}
